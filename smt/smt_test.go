28b114be649a028217f366dfc477f2b3c363849b
Andrew Archibald <andrew@aarchibald.com>
2019-11-10 01:16:00 -0700

    Experiment with Z3 SMT solver

---
+++ smt/smt_test.go
@@ -0,0 +1,35 @@
+package smt
+
+import (
+	"testing"
+
+	"github.com/mitchellh/go-z3"
+)
+
+func TestASTAdd(t *testing.T) {
+	config := z3.NewConfig()
+	defer config.Close()
+
+	ctx := z3.NewContext(config)
+	defer ctx.Close()
+
+	x := ctx.Const(ctx.Symbol("x"), ctx.IntSort())
+	v10 := ctx.Int(10, ctx.IntSort())
+	v7 := ctx.Int(7, ctx.IntSort())
+	v0 := ctx.Int(0, ctx.IntSort())
+
+	eq := x.Mul(v10).Sub(v7).Eq(v0)
+
+	s := ctx.NewSolver()
+	defer s.Close()
+	s.Assert(eq)
+
+	result := s.Check()
+	if result != z3.True {
+		t.Fatalf("bad: %s", result)
+	}
+
+	m := s.Model()
+	defer m.Close()
+	t.Logf("\nModel:\n%s", m.String())
+}
