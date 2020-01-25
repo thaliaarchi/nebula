503a550c3b0dc3eba113d60fba14b4036d573b51
Andrew Archibald <andrew@aarchibald.com>
2020-01-25 02:04:05 +0100

    Rename external definitions to lib.c

--- build/compile.sh
+++ build/compile.sh
@@ -4,7 +4,7 @@ program="$1"
 out="$2"
 
 ../nebula "$program" llvm > "$out.ll"
-clang -S -emit-llvm ../codegen/lib/io.c
-llvm-link -o "$out.o" "$out.ll" io.ll
+clang -S -emit-llvm ../codegen/lib/lib.c
+llvm-link -o "$out.o" "$out.ll" lib.ll
 llc "$out.o"
 clang -o "$out" "$out.o.s"
