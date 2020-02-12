88b04e9721ffd98a2c574f0ea445560e50457bfe
Andrew Archibald <andrew@aarchibald.com>
2020-02-12 16:30:00 +0100

    Build and test Rosetta programs

--- Makefile
+++ Makefile
@@ -1,6 +1,15 @@
 all: build/99_bottles build/ascii build/ascii4 build/caesar \
 	build/collatz build/factorial build/fib build/fizz_buzz \
-	build/hello_world build/interpret build/pi
+	build/hello_world build/interpret build/pi \
+	build/rosetta/add build/rosetta/binary build/rosetta/fib  \
+	build/rosetta/fibrec build/rosetta/fizz_buzz build/rosetta/freq \
+	build/rosetta/harshad build/rosetta/langstons_ant \
+	build/rosetta/octal build/rosetta/shell_sort \
+	build/rosetta/term_cursor build/rosetta/while
+
+build/rosetta/%: programs/rosetta/%.ws nebula compile codegen/ext/ext.c
+	@mkdir -p build/rosetta
+	./compile $< $@
 
 build/%: programs/%.ws nebula compile codegen/ext/ext.c
 	./compile $< $@
@@ -10,6 +19,10 @@ build/%: programs/%.out.ws nebula compile codegen/ext/ext.c
 
 build/interpret: programs/interpret.out.ws nebula compile codegen/ext/ext.c
 	./compile $< $@ -O3 -heap=1000000
+
+build/rosetta/langstons_ant: programs/rosetta/langstons_ant.ws nebula compile codegen/ext/ext.c
+	@mkdir -p build/rosetta
+	./compile $< $@ -O3 -heap=10004
 
 clean:
 	rm -rf build
--- test.sh
+++ test.sh
@@ -5,24 +5,69 @@ wspace=wspace
 
 make all
 
-echo "99_bottles:";  diff <(build/99_bottles)  <("$wspace" programs/99_bottles.out.ws)
-echo "ascii:";       diff <(build/ascii)       <("$wspace" programs/ascii.out.ws)
-echo "ascii4:";      diff <(build/ascii4)      <("$wspace" programs/ascii4.out.ws)
-echo "fizz_buzz:";   diff <(build/fizz_buzz)   <("$wspace" programs/fizz_buzz.out.ws)
-echo "hello_world:"; diff <(build/hello_world) <("$wspace" programs/hello_world.ws)
+function compare() {
+  binary="$1"
+  program="$2"
+  echo "$binary:"
+  diff <("build/$binary") <("$wspace" "programs/$program")
+}
 
-echo "collatz 5:";    diff <(echo 5   | build/collatz)   <(echo 5   | "$wspace" programs/collatz.out.ws)
-echo "collatz 10:";   diff <(echo 10  | build/collatz)   <(echo 10  | "$wspace" programs/collatz.out.ws)
-echo "collatz 100:";  diff <(echo 100 | build/collatz)   <(echo 100 | "$wspace" programs/collatz.out.ws)
-echo "factorial 10:"; diff <(echo 10  | build/factorial) <(echo 10  | "$wspace" programs/factorial.out.ws)
-echo "pi 5:";         diff <(echo 5   | build/pi)        <(echo 5   | "$wspace" programs/pi.out.ws)
+function compare_arg() {
+  binary="$1"
+  program="$2"
+  args="$3"
+  echo "$binary $args:"
+  diff <(echo "$args" | "build/$binary") <(echo "$args" | "$wspace" "programs/$program")
+}
 
-cp programs/interpret.out.ws run.ws
-printf '\0' >> run.ws
-cat programs/hello_world.ws >> run.ws
-printf '\0' >> run.ws
+function compare_file() {
+  binary="$1"
+  program="$2"
+  file="$3"
+  echo "$binary $file:"
+  diff <("build/$binary" < "$file") <("$wspace" "programs/$program" < "$file")
+}
+
+function compare_head() {
+  binary="$1"
+  program="$2"
+  n="$3"
+  echo "$binary $n:"
+  diff <("build/$binary" | head -n "$n") <("$wspace" "programs/$program" | head -n "$n")
+}
+
+compare      99_bottles            99_bottles.out.ws
+compare      ascii                 ascii.out.ws
+compare      ascii4                ascii4.out.ws
+compare_arg  collatz               collatz.out.ws           5
+compare_arg  collatz               collatz.out.ws           10
+compare_arg  collatz               collatz.out.ws           100
+compare_arg  factorial             factorial.out.ws         10
+compare      fizz_buzz             fizz_buzz.out.ws
+compare      hello_world           hello_world.ws
+compare_arg  pi                    pi.out.ws                5
+
+compare_arg  rosetta/add           rosetta/add.ws           "42
+314"
+compare_head rosetta/binary        rosetta/binary.ws        100
+compare_head rosetta/fib           rosetta/fib.ws           47
+compare_arg  rosetta/fibrec        rosetta/fibrec.ws        20
+compare      rosetta/fizz_buzz     rosetta/fizz_buzz.ws
+compare      rosetta/harshad       rosetta/harshad.ws
+compare_head rosetta/octal         rosetta/octal.ws         100
+compare_file rosetta/shell_sort    rosetta/shell_sort.ws    programs/rosetta/shell_sort.in
+compare      rosetta/term_cursor   rosetta/term_cursor.ws
+compare      rosetta/while         rosetta/while.ws
+
+compare      rosetta/langstons_ant rosetta/langstons_ant.ws
+compare_file rosetta/freq          rosetta/freq.ws          programs/rosetta/freq.ws
+
+cp programs/interpret.out.ws tmp.ws
+printf '\0' >> tmp.ws
+cat programs/hello_world.ws >> tmp.ws
+printf '\0' >> tmp.ws
 
 echo "hello_world:"
-time build/interpret < run.ws
-time "$wspace" programs/interpret.out.ws < run.ws
-rm run.ws
+time build/interpret < tmp.ws
+time "$wspace" programs/interpret.out.ws < tmp.ws
+rm tmp.ws
