b5467c91529f377ac6da92ab6142c306253120a2
Andrew Archibald <andrew@aarchibald.com>
2020-02-18 10:28:30 +0100

    Redesign IR structures and interfaces

--- ir/instructions.go
+++ ir/instructions.go
@@ -1,153 +1,224 @@
 package ir // import "github.com/andrewarchi/nebula/ir"
 
-import "math/big"
+import (
+	"fmt"
+	"math/big"
+)
 
-// Val can be StackVal, HeapVal, ConstVal, or AddrVal. Two vals can be
-// compared by address for equality.
-type Val interface {
-	val()
-}
-
-// SSAVal is an SSA register.
-type SSAVal struct {
-	ID int // TODO apparently not uniquely addressable when empty
-}
-
-// ConstVal is a constant value such as from push or an expression with
-// constant operands.
-type ConstVal struct{ Int *big.Int }
-
-// PhiVal represents an SSA Φ function and stores the set of values it
-// could be.
-type PhiVal struct {
-	Refs []PhiRef
-}
-
-// PhiRef is a reference in a PhiVal.
-type PhiRef struct {
-	Val   *Val
-	Block *BasicBlock
-}
-
-// Node can be any expr or stmt type.
+// Node is a non-branching instruction in a basic block.
 type Node interface {
 	node()
 }
 
-// Expr represents an SSA expression that produces a value.
+// Expr is an expression that produces a value.
 type Expr interface {
 	Node
-	exprNode()
+	Value
+	expr()
 }
 
-// Stmt represents an SSA statement that does not produce a value.
+// Stmt is a statement that does not produce a value.
 type Stmt interface {
 	Node
-	stmtNode()
+	stmt()
 }
 
-// Terminator is any control flow statement. Valid types are CallStmt,
-// JmpStmt, JmpCondStmt, RetStmt, and ExitStmt.
+// Terminator is a branching instruction that terminates a basic block.
 type Terminator interface {
-	Node
-	termNode()
+	terminator()
 }
 
-// BinaryExpr evaluates a binary operation. Valid operator are add, sub,
-// mul, div, and mod.
+// Value is an SSA value with a definition and uses.
+type Value interface {
+	ValueDef() *ValueDef
+	value()
+}
+
+// ValueDef is the definition of a value.
+type ValueDef struct {
+	Uses []*ValueUse
+}
+
+// ValueUse is an edge between a value and a user.
+type ValueUse struct {
+	Val     Value
+	User    User
+	Operand int
+}
+
+// User is an expression that has an operand that uses a value.
+type User interface {
+	Operand(int) *ValueUse
+	SetOperand(int, *ValueUse)
+	NumOperand() int
+	user()
+}
+
+// ConstVal is a constant integer value.
+type ConstVal struct {
+	Def *ValueDef
+	Int *big.Int
+}
+
+func (*ConstVal) value() {}
+
+// PhiExpr is an SSA φ node. It contains a list of edges to values in
+// predecessor blocks.
+type PhiExpr struct {
+	Def   *ValueDef
+	Edges []PhiEdge
+}
+
+// PhiEdge is a reference to a value in a predecessor block.
+type PhiEdge struct {
+	ValueUse *ValueUse
+	Block    *BasicBlock
+}
+
+func (*PhiExpr) node()  {}
+func (*PhiExpr) expr()  {}
+func (*PhiExpr) value() {}
+func (*PhiExpr) user()  {}
+
+// BinaryExpr is an expression with two operands.
 type BinaryExpr struct {
-	Op     OpType
-	Assign *Val
-	LHS    *Val
-	RHS    *Val
+	Def *ValueDef
+	Op  OpType
+	LHS *ValueUse
+	RHS *ValueUse
 }
 
-// UnaryExpr evaluates a unary operation. Valid operator is neg.
+func (*BinaryExpr) node()  {}
+func (*BinaryExpr) expr()  {}
+func (*BinaryExpr) value() {}
+func (*BinaryExpr) user()  {}
+
+// UnaryExpr is an expression with one operand.
 type UnaryExpr struct {
-	Op     OpType
-	Assign *Val
-	Val    *Val
+	Def *ValueDef
+	Op  OpType
+	Val *ValueUse
 }
 
-// LoadStackExpr loads from a position under the current stack frame. A
-// position of 1 is the top of the stack.
+func (*UnaryExpr) node()  {}
+func (*UnaryExpr) expr()  {}
+func (*UnaryExpr) value() {}
+func (*UnaryExpr) user()  {}
+
+// LoadStackExpr is an expression that loads a value from under the
+// current stack frame. A position of 1 is the top of the stack.
 type LoadStackExpr struct {
-	Assign *Val
-	Pos    int
+	Def *ValueDef
+	Pos int
 }
 
-// LoadHeapExpr evaluates a retrieve operation.
+func (*LoadStackExpr) node()  {}
+func (*LoadStackExpr) expr()  {}
+func (*LoadStackExpr) value() {}
+
+// LoadHeapExpr is an expression that loads a value at an address
+// from the heap.
 type LoadHeapExpr struct {
-	Assign *Val
-	Addr   *Val
+	Def  *ValueDef
+	Addr *ValueUse
 }
 
-// StoreHeapStmt evaluates a store operation.
+func (*LoadHeapExpr) node()  {}
+func (*LoadHeapExpr) expr()  {}
+func (*LoadHeapExpr) value() {}
+func (*LoadHeapExpr) user()  {}
+
+// StoreHeapStmt is a statement that stores a value at an address
+// in the heap.
 type StoreHeapStmt struct {
-	Addr *Val
-	Val  *Val
+	Addr *ValueUse
+	Val  *ValueUse
 }
 
-// CheckStackStmt asserts the stack length.
+func (*StoreHeapStmt) node() {}
+func (*StoreHeapStmt) stmt() {}
+func (*StoreHeapStmt) user() {}
+
+// CheckStackStmt is a statement that asserts the stack length.
 type CheckStackStmt struct {
 	Access int
 }
 
-// PrintStmt prints a value. Valid operations are printc and printi.
+func (*CheckStackStmt) node() {}
+func (*CheckStackStmt) stmt() {}
+
+// PrintStmt is an expression that prints a value to stdout.
 type PrintStmt struct {
 	Op  OpType
-	Val *Val
+	Val *ValueUse
 }
 
-// ReadExpr reads a value from stdin. Valid operations are readc and
-// readi.
+func (*PrintStmt) node() {}
+func (*PrintStmt) stmt() {}
+func (*PrintStmt) user() {}
+
+// ReadExpr is an expression that reads a value from stdin.
 type ReadExpr struct {
-	Op     OpType
-	Assign *Val
+	Def *ValueDef
+	Op  OpType
 }
 
-// FlushStmt flushes stdout.
+func (*ReadExpr) node()  {}
+func (*ReadExpr) expr()  {}
+func (*ReadExpr) value() {}
+
+// FlushStmt is a statement that flushes stdout.
 type FlushStmt struct{}
 
-// CallStmt represents a call.
-type CallStmt struct {
+func (*FlushStmt) node() {}
+func (*FlushStmt) stmt() {}
+
+// CallTerm is terminator that pushes the current location to the call
+// stack, then jumps to the callee.
+type CallTerm struct {
 	Dest *BasicBlock
+	Next *BasicBlock
 }
 
-// JmpStmt unconditionally jumps to a block. Valid instructions are jmp
-// and fallthrough.
-type JmpStmt struct {
+func (*CallTerm) node()       {}
+func (*CallTerm) terminator() {}
+
+// JmpTerm is a terminator that unconditionally jumps to a block.
+type JmpTerm struct {
 	Op   OpType
 	Dest *BasicBlock
 }
 
-// JmpCondStmt conditionally jumps to a block based on a value. Valid
-// instructions are jz and jn.
-type JmpCondStmt struct {
+func (*JmpTerm) node()       {}
+func (*JmpTerm) terminator() {}
+
+// JmpCondTerm is a terminator that conditionally jumps to one of
+// two blocks.
+type JmpCondTerm struct {
 	Op   OpType
-	Cond *Val
+	Cond *ValueUse
 	Then *BasicBlock
 	Else *BasicBlock
 }
 
-// RetStmt represents a ret.
-type RetStmt struct{}
+func (*JmpCondTerm) node()       {}
+func (*JmpCondTerm) terminator() {}
+func (*JmpCondTerm) user()       {}
 
-// ExitStmt represents an end.
-type ExitStmt struct{}
+// RetTerm is a terminator that returns to the caller.
+type RetTerm struct{}
 
-// OpType is the kind of operation of a node.
+func (*RetTerm) node()       {}
+func (*RetTerm) terminator() {}
+
+// ExitTerm is a terminator that exits the program.
+type ExitTerm struct{}
+
+func (*ExitTerm) node()       {}
+func (*ExitTerm) terminator() {}
+
+// OpType is the kind of operator in a node.
 type OpType uint8
-
-// ValEq returns whether the two vals reference the same definition.
-func ValEq(a, b *Val) bool {
-	return a != nil && b != nil && *a == *b
-}
-
-// AddIncoming adds a val for an incoming edge to the phi node.
-func (phi *PhiVal) AddIncoming(val *Val, block *BasicBlock) {
-	phi.Refs = append(phi.Refs, PhiRef{val, block})
-}
 
 // Operators for nodes with multiple types.
 const (
@@ -158,14 +229,14 @@ const (
 	Mul
 	Div
 	Mod
-	Neg
-
 	Shl
 	LShr
 	AShr
 	And
 	Or
 	Xor
+
+	Neg
 
 	Printc
 	Printi
@@ -229,36 +300,145 @@ func (op OpType) String() string {
 	return "illegal"
 }
 
-func (SSAVal) val()   {}
-func (ConstVal) val() {}
-func (PhiVal) val()   {}
+// AddIncoming adds a val for an incoming edge to the phi node.
+func (phi *PhiExpr) AddIncoming(use *ValueUse, block *BasicBlock) {
+	phi.Edges = append(phi.Edges, PhiEdge{use, block})
+}
 
-func (BinaryExpr) node()     {}
-func (UnaryExpr) node()      {}
-func (LoadStackExpr) node()  {}
-func (LoadHeapExpr) node()   {}
-func (StoreHeapStmt) node()  {}
-func (CheckStackStmt) node() {}
-func (PrintStmt) node()      {}
-func (ReadExpr) node()       {}
-func (FlushStmt) node()      {}
-func (CallStmt) node()       {}
-func (JmpStmt) node()        {}
-func (JmpCondStmt) node()    {}
-func (RetStmt) node()        {}
-func (ExitStmt) node()       {}
+// Value methods
 
-func (BinaryExpr) exprNode()     {}
-func (UnaryExpr) exprNode()      {}
-func (LoadStackExpr) exprNode()  {}
-func (LoadHeapExpr) exprNode()   {}
-func (StoreHeapStmt) stmtNode()  {}
-func (CheckStackStmt) stmtNode() {}
-func (PrintStmt) stmtNode()      {}
-func (ReadExpr) exprNode()       {}
-func (FlushStmt) stmtNode()      {}
-func (CallStmt) termNode()       {}
-func (JmpStmt) termNode()        {}
-func (JmpCondStmt) termNode()    {}
-func (RetStmt) termNode()        {}
-func (ExitStmt) termNode()       {}
+// ValueDef returns the definition information of a Value.
+func (c *ConstVal) ValueDef() *ValueDef { return c.Def }
+
+// ValueDef returns the definition information of a Value.
+func (phi *PhiExpr) ValueDef() *ValueDef { return phi.Def }
+
+// ValueDef returns the definition information of a Value.
+func (b *BinaryExpr) ValueDef() *ValueDef { return b.Def }
+
+// ValueDef returns the definition information of a Value.
+func (u *UnaryExpr) ValueDef() *ValueDef { return u.Def }
+
+// ValueDef returns the definition information of a Value.
+func (l *LoadStackExpr) ValueDef() *ValueDef { return l.Def }
+
+// ValueDef returns the definition information of a Value.
+func (l *LoadHeapExpr) ValueDef() *ValueDef { return l.Def }
+
+// ValueDef returns the definition information of a Value.
+func (r *ReadExpr) ValueDef() *ValueDef { return r.Def }
+
+// User methods
+
+// Operand returns the nth operand, panicking if out of range.
+func (phi *PhiExpr) Operand(n int) *ValueUse {
+	if n < 0 || n > len(phi.Edges) {
+		panic(invalidOperand(n))
+	}
+	return phi.Edges[n].ValueUse
+}
+
+// Operand returns the nth operand, panicking if out of range.
+func (b *BinaryExpr) Operand(n int) *ValueUse { return getOperand2(n, b.LHS, b.RHS) }
+
+// Operand returns the nth operand, panicking if out of range.
+func (u *UnaryExpr) Operand(n int) *ValueUse { return getOperand1(n, u.Val) }
+
+// Operand returns the nth operand, panicking if out of range.
+func (l *LoadHeapExpr) Operand(n int) *ValueUse { return getOperand1(n, l.Addr) }
+
+// Operand returns the nth operand, panicking if out of range.
+func (s *StoreHeapStmt) Operand(n int) *ValueUse { return getOperand2(n, s.Addr, s.Val) }
+
+// Operand returns the nth operand, panicking if out of range.
+func (p *PrintStmt) Operand(n int) *ValueUse { return getOperand1(n, p.Val) }
+
+// Operand returns the nth operand, panicking if out of range.
+func (j *JmpCondTerm) Operand(n int) *ValueUse { return getOperand1(n, j.Cond) }
+
+// SetOperand sets the nth operand, panicking if out of range.
+func (phi *PhiExpr) SetOperand(n int, use *ValueUse) {
+	if n < 0 || n > len(phi.Edges) {
+		panic(invalidOperand(n))
+	}
+	phi.Edges[n].ValueUse = use
+}
+
+// SetOperand sets the nth operand, panicking if out of range.
+func (b *BinaryExpr) SetOperand(n int, use *ValueUse) { setOperand2(n, &b.LHS, &b.RHS, use) }
+
+// SetOperand sets the nth operand, panicking if out of range.
+func (u *UnaryExpr) SetOperand(n int, use *ValueUse) { setOperand1(n, &u.Val, use) }
+
+// SetOperand sets the nth operand, panicking if out of range.
+func (l *LoadHeapExpr) SetOperand(n int, use *ValueUse) { setOperand1(n, &l.Addr, use) }
+
+// SetOperand sets the nth operand, panicking if out of range.
+func (s *StoreHeapStmt) SetOperand(n int, use *ValueUse) { setOperand2(n, &s.Addr, &s.Val, use) }
+
+// SetOperand sets the nth operand, panicking if out of range.
+func (p *PrintStmt) SetOperand(n int, use *ValueUse) { setOperand1(n, &p.Val, use) }
+
+// SetOperand sets the nth operand, panicking if out of range.
+func (j *JmpCondTerm) SetOperand(n int, use *ValueUse) { setOperand1(n, &j.Cond, use) }
+
+// NumOperand returns the number of operands.
+func (phi *PhiExpr) NumOperand() int { return len(phi.Edges) }
+
+// NumOperand returns the number of operands.
+func (b *BinaryExpr) NumOperand() int { return 2 }
+
+// NumOperand returns the number of operands.
+func (u *UnaryExpr) NumOperand() int { return 1 }
+
+// NumOperand returns the number of operands.
+func (l *LoadHeapExpr) NumOperand() int { return 1 }
+
+// NumOperand returns the number of operands.
+func (s *StoreHeapStmt) NumOperand() int { return 2 }
+
+// NumOperand returns the number of operands.
+func (p *PrintStmt) NumOperand() int { return 1 }
+
+// NumOperand returns the number of operands.
+func (j *JmpCondTerm) NumOperand() int { return 1 }
+
+func getOperand1(n int, operand0 *ValueUse) *ValueUse {
+	if n != 0 {
+		panic(invalidOperand(n))
+	}
+	return operand0
+}
+
+func getOperand2(n int, operand0, operand1 *ValueUse) *ValueUse {
+	switch n {
+	case 0:
+		return operand0
+	case 1:
+		return operand1
+	default:
+		panic(invalidOperand(n))
+	}
+}
+
+func setOperand1(n int, operand0 **ValueUse, use *ValueUse) {
+	if n != 0 {
+		panic(invalidOperand(n))
+	}
+	*operand0 = use
+}
+
+func setOperand2(n int, operand0, operand1 **ValueUse, use *ValueUse) {
+	switch n {
+	case 0:
+		*operand0 = use
+	case 1:
+		*operand1 = use
+	default:
+		panic(invalidOperand(n))
+	}
+}
+
+func invalidOperand(n int) string {
+	return fmt.Sprintf("ir: invalid operand index: %d", n)
+}
